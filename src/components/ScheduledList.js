04bfe431512a9d58dfe5f2a77652f84429cfd43a
cgsimmons <chris.simmons.alt@gmail.com>
2016-12-13 15:49:11 -0800

    Change styles

--- src/components/ScheduledList.js
+++ src/components/ScheduledList.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 export default class ScheduledList extends React.Component {
 
+
   render(){
     return (
+
       <div className='ScheduledList dashboard-panel main-panel'>
-        <p>{this.props.list.interval}</p>
+        <h3>{this.props.list.title}</h3>
+        <span className="ReactTags__tag">
+          <a className="ReactTags__remove delete">×</a>
+        </span>
+        <p>{this.props.list.interval.replace('y', 'i')}ly from {this.props.list.startDate.slice(0,10)}</p>
+        <hr/>
+
       </div>
     );
   }
