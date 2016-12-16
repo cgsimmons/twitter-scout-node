ba11e00e9f70027b032d131a8c9eb9132e76a2b8
cgsimmons <chris.simmons.alt@gmail.com>
2016-12-15 17:33:16 -0800

    fix index

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -6,7 +6,7 @@ export default class IndexPage extends React.Component {
       <div className="home">
         <h1 className='logo-title'>Twitter Scout</h1>
         <p className='logo-tagline'>twitter management made easy</p>
-        <a href="/auth/twitter" className="login-button under-logo">Log in with Facebook</a>
+        <a href="/auth/twitter" className="login-button under-logo">Sign in with Twitter</a>
         <div className="logo-box">
           <img className="logo" src="/img/scope2.jpg"/>
         </div>
