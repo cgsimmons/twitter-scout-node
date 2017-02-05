3b3df808be2c4e729cf489129f3415e219268304
cgsimmons <chris.simmons.alt@gmail.com>
2017-02-06 00:44:43 +0900

    compress images

--- src/common/components/IndexPage.js
+++ src/common/components/IndexPage.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
       <p className="logo-tagline">twitter management made easy</p>
       <a href="/auth/twitter" className="submit-button under-logo">Sign in with Twitter</a>
       <div className="logo-box">
-        <img className="logo" src="/img/scope2.jpg" alt="Logo" />
+        <img className="logo" src="/img/scope2_sm.jpg" alt="Logo" />
       </div>
     </div>
   );
